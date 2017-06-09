05786510b8bc5bd3082761d3d83e5ff9dd46a0d1
tvisenti <tvisenti@student.42.fr>
2017-06-09 16:04:37 +0200

    [Theta in other file]

--- first.py
+++ first.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 
 import sys, re
-
-theta0 = 0
-theta1 = 0
+from theta import *
 
 def main():
 	if (len(sys.argv) == 1):
 		km = input("Enter mileage: ")
-		if (km.isdigit()):
-			print (theta0 + (theta1 * int(km)))
-		else:
-			print ("Mileage is not a positive int")
+		try:
+			val = int(km)
+			if (val < 0):
+				print("That's not a positive int!")
+				return
+		except ValueError:
+			print("That's not an int!")
+			return
+		result = (theta0 + (theta1 * int(km)))
+		print ("Result: ", result)
+
 	elif (len(sys.argv) > 1):
 		print ("Too few arguments")
 
--- second.py
+++ second.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
 
 import sys, re
-import matplotlib.pyplot as plt
 
-def display():
-    # plt.scatter(lstLeft, lstRight, s = 15)
-    # plt.title('ft_linear_regression')
-    # plt.xlabel(lstPattern[0])
-    # plt.ylabel(lstPattern[1])
-    # plt.show()
+def createNewFile(theta0, theta1):
+    try:
+        file = open("theta.py",'w+')
+        file.write("theta0 = ")
+        file.write(str(theta0))
+        file.write("\n")
+        file.write("theta1 = ")
+        file.write(str(theta1))
+        file.write("\n")
+        file.close()
+    except:
+        print('Something went wrong! Can\'t tell what?')
+        sys.exit(0)
     return
 
 def calculLinearRegression(lstLeft, lstRight, lstPattern):
-    tmp0 = 0
-    tmp1 = 0
-    theta0 = 0
-    theta1 = 0
-    learningRate = 0.1
-    i = 0
-
-    # miss learningRate (calcul gradient descent)
+    sum0, sum1 = 0, 0
+    tmpTheta0, tmpTheta1 = 0, 0
+    learningRate, i = 0.1, 0
     # miss other loop
     while (i < len(lstLeft)):
-        tmp0 = (theta0 + (theta1 * lstLeft[i])) - lstRight[i]
-        tmp1 = ((theta0 + (theta1 * lstLeft[i])) - lstRight[i]) * lstLeft[i]
-        theta0 = theta0 - (learningRate * tmp0)
-        theta1 = theta1 - (learningRate * tmp1)
+        sum0 = (tmpTheta0 + (tmpTheta1 * lstLeft[i])) - lstRight[i]
+        sum1 = ((tmpTheta0 + (tmpTheta1 * lstLeft[i])) - lstRight[i]) * lstLeft[i]
+        tmpTheta0 -= learningRate * sum0
+        tmpTheta1 -= learningRate * sum1
         i += 1
     i = 0
-    print ("theta0: ", theta0)
-    print ("theta1: ", theta1)
-
-
+    print ("tmpTheta0: ", tmpTheta0)
+    print ("tmpTheta1: ", tmpTheta1)
+    print ("sum0", sum0)
+    createNewFile(tmpTheta0, tmpTheta1)
     return
 
 def createList(arg):
